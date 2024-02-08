6b058740d30b01d1cafba5b281424d98d8350537
msquaredds <alex.melesko@msquaredds.com>
2024-02-08 11:04:59 -0500

    Changing register_user to have session_states in the callback

--- StreamlitAuth/Authenticate.py
+++ StreamlitAuth/Authenticate.py
@@ -216,9 +216,6 @@ class Authenticate(object):
                                      'email': enc_email.ciphertext,
                                      'password': password}
 
-        st.write(self.user_credentials)
-        st.stop()
-
         # if we had the name preauthorized, remove it from that list
         if preauthorization:
             self.preauthorized.remove(email)
