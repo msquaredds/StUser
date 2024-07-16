24d9dc962dfe3b25e4c004edaba2e700948c16ba
msquaredds <alex.melesko@msquaredds.com>
2024-07-16 10:48:32 -0400

    Adding the end of the forgot_password code

--- StreamlitAuth/Authenticate.py
+++ StreamlitAuth/Authenticate.py
@@ -2391,6 +2391,7 @@ class Authenticate(object):
         if isinstance(password_store_function, str):
             if password_store_function.lower() == 'bigquery':
                 db = BQTools()
+                st.write("password_store_args", password_store_args)
                 error = db.update_password(**password_store_args)
             else:
                 error = (
